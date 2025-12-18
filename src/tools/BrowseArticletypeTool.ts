5ac095417b0b6ab43ed7ceeb6fe1edb613b0ebc1
Barry Dobyns <bdobyns@mms.org>
2025-12-18 12:24:21 -0500

    COM-4690 add a browse tool

---
+++ src/tools/BrowseArticletypeTool.ts
@@ -0,0 +1,86 @@
+import { MCPTool } from "mcp-framework";
+import { z } from "zod";
+
+interface BrowsearticletypeInput {
+  context: string
+  articleType: string;
+}
+
+class BrowsearticletypeTool extends MCPTool<BrowsearticletypeInput> {
+  name = "BrowseArticleType";
+  description = "Browsearticletype tool description";
+
+  schema = {
+    context: {
+      type: z.enum(["nejm","catalyst","evidence","clinician","nejm-ai"]),
+      description: "the journal to query against",
+    },
+    articleType: {
+      type: z.enum(["NEJM Images in Clinical Medicine",
+"Audio",
+"Video",
+"Review Article",
+"Case Study",
+"Journal Watch",
+"Guideline Watch",
+"Clinical Conversations",
+"Drug Watch",
+"Graphical Research Summary",
+"NEJM Quick Take",
+"Year in Review",
+"Letter to Readers",
+"AIDS Watch",
+"Antiretroviral Rounds",
+"Case History",
+"Clinical Practice Guideline Watch",
+"Clinical Spotlight",
+"Correction",
+"Editor's Choice",
+"Editorial",
+"Feature",
+"From the Blogs",
+"General Medicine",
+"Landmark Article",
+"Medical News",
+"Meeting Notes",
+"Meeting Report",
+"News in Context",
+"Patient Information",
+"Practice Watch",
+"Research Notes",
+"Top General Medicine Stories",
+"Top Story",
+"Vaccine Watch",
+"Question of the Week",
+"NEJM Image Challenge",
+"NEJM Research Summary",]),
+      description: "which article type to browse",
+    },
+
+  };
+
+  async execute(input: SimpleQueryInput) {
+    let baseURL = process.env.APIHOST+'/api/v1/simple';
+    const params = {
+      context: input.context,
+      articleType: input.articleType
+      sortBy: "pubdate-descending"
+    };
+    try {
+    const results = await axios.get(baseURL,
+      params: params,
+      headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json',
+         'x-nejmgroup-correlation-id': req.correlationId,                
+         'apikey': process.env.APIKEY,
+         'apiuser': process.env.APIUSER
+      }
+    });
+  } catch (error) {
+    console.error("Error executing articleType browse:", error);
+    // throw new Error("Failed to execute articleType browse");
+  }
+}
+
+export default BrowsearticletypeTool;
